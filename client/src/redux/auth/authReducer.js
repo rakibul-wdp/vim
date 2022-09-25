6f5bd739ea08ded6718f1277d99499ecbd2bd16f
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-26 04:05:00 +0600

    create auth reducer in redux

---
+++ client/src/redux/auth/authReducer.js
@@ -0,0 +1,60 @@
+import {
+  AUTH_ERROR,
+  AUTH_LOGIN_SUCCESS,
+  AUTH_REGISTER_SUCCESS,
+  AUTH_LOGOUT
+} from './auth.type';
+
+const initialState = {
+  isRegistered: false,
+  isAuth: !!localStorage.getItem('token'),
+  userData: {},
+  isError: false,
+  ErrorMsg: '',
+};
+
+export const authReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case AUTH_REGISTER_SUCCESS: {
+      return {
+        ...state,
+        isRegistered: true,
+        isError: false,
+        isAuth: true,
+        userData: true,
+        ErrorMsg: '',
+      };
+    }
+    case AUTH_LOGIN_SUCCESS: {
+      return {
+        ...state,
+        isAuth: payload,
+        isError: false,
+        userData: payload.user,
+        ErrorMsg: '',
+      };
+    }
+    case AUTH_ERROR: {
+      return {
+        ...state,
+        isRegistered: false,
+        isAuth: false,
+        isError: true,
+        ErrorMsg: payload,
+      };
+    }
+    case AUTH_LOGOUT: {
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        isRegistered: false,
+        isAuth: false,
+        isError: false,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
