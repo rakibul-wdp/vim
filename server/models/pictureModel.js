fda2875c0c976bd1849c962df8ad71b44f1e713f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-10 05:11:41 +0600

    create pictureModel schema

---
+++ server/models/pictureModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const pictureSchema = new mongoose.Schema({
+  name: { type: String },
+  url: {
+    type: String,
+    required: true
+  },
+  userId: { type: mongoose.Types.ObjectId, ref: 'user' }
+});
+
+const Picture = mongoose.model('picture', pictureSchema);
+
+module.exports = Picture;
